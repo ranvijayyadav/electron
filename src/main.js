09222a533c7c43f0b25bfac8b9bbf4e5ce8877be
Ranvijay <Ranvijayva@gmail.com>
2016-10-10 01:07:45 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const electron =require('electron');
+const app=electron.app;
+const BrowserWindow=electron.BrowserWindow;
+
+let mainWindow;
+app.on('ready',_=>{
+    mainWindow=new BrowserWindow({
+        height:400,
+        width:400
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/countdown.html`);
+    mainWindow.on('closed',_=>{
+        console.log("closed");
+        mainWindow=null;
+    });
+});
